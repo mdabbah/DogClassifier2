c56b09f5205db23a5abb72c1b6935f75323e40e8
mdabbah <34287255+mdabbah@users.noreply.github.com>
2018-12-21 21:27:19 +0200

    initial commit

---
+++ app/src/main/java/com/example/mdabb/dogclassifier/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.mdabb.dogclassifier;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+//        mRecyclerView.setHasFixedSize(true);
+//
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyRecyclerViewAdapter(null);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // setting up response to camera launch button
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCameraButtonClick(view);
+
+            }
+        });
+    }
+
+    public void onCameraButtonClick(View view){
+        final Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/mdabb/dogclassifier/MyRecyclerViewAdapter.java
@@ -0,0 +1,26 @@
+package com.example.mdabb.dogclassifier;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+class MyRecyclerViewAdapter extends RecyclerView.Adapter {
+    public MyRecyclerViewAdapter(Object p0) {
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
